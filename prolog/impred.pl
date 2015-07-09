b88175d94c9abcdcaf4ac77e7c23c06e80dd1d06
Boaz Rosenan <brosenan@gmail.com>
2015-07-09 15:01:31 +0300

    impred-evaluator

--- prolog/impred.pl
+++ prolog/impred.pl
@@ -1,1 +1,19 @@
 :- [service, cedalion].
+
+go :- read(Cmd),
+      catch(handleCmd(Cmd, Continue), Error, handleError(Error)),
+      Continue = yes,
+      go.
+
+handleCmd(end_of_file, no) :- !.
+handleCmd(eval(Res, Impred), yes) :- !, evalImpred(Res, Impred).
+handleCmd(Cmd, _) :- throw(bad_command(Cmd)).
+
+handleError(Error) :- write('! '), write(Error), nl.
+
+evalImpred(Res, Impred) :-
+    forall('/impred#solve'(Impred,Res,_,Resp), writeResponse(Resp)),
+    write('.'), nl.
+
+writeResponse('/impred#solution'(Value)) :- write(': '), write_term(Value, []), nl.
+writeResponse('/impred#continuation'(Task,RetVal,RetType,Continuation,Res,ResultType)) :- write('? '), write_term(cont(Task, RetVal, Continuation, Res), []), nl.
