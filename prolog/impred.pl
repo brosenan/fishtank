930ba312f218b65bcca4285d75c38f67cd7c2a0c
Boaz Rosenan <brosenan@gmail.com>
2015-07-10 13:32:07 +0300

    cont command

--- prolog/impred.pl
+++ prolog/impred.pl
@@ -1,22 +1,47 @@
 :- [service, cedalion].
+:- dynamic storedTerm/2.
 
 go :- read(Cmd),
-      catch(handleCmd(Cmd, Continue), Error, handleError(Error)),
+      catch(mustSucceed(handleCmd(Cmd, Continue)), Error, handleError(Error)),
       Continue = yes,
       go.
 
 handleCmd(end_of_file, no) :- !.
 handleCmd(eval(Res, Impred), yes) :- !, evalImpred(Res, Impred).
+handleCmd(cont(ID, RetVal), yes) :- !, continue(ID, RetVal).
 handleCmd(Cmd, _) :- throw(bad_command(Cmd)).
 
-handleError(Error) :- write('! '), write(Error), nl.
+handleError(Error) :- write('! '), writeTerm(Error), nl.
 
 evalImpred(Res, Impred) :-
     forall('/impred#solve'(Impred,Res,_,Resp), writeResponse(Resp)),
     write('.'), nl.
 
-writeResponse('/impred#solution'(Value)) :- write(': '), write_term(Value, []), nl.
-writeResponse('/impred#continuation'(Task,RetVal,RetType,Continuation,Res)) :- write('? '), write_term(cont(Task, RetVal, Continuation, Res), []), nl.
+writeResponse('/impred#solution'(Value)) :- write(': '), writeTerm(Value), nl.
+writeResponse('/impred#continuation'(Task,RetVal,RetType,Continuation,Res)) :- 
+    storeTerm(cont(RetVal, Continuation, Res), ID),
+    write('? '), write(ID), write(' '), writeTerm(Task), nl.
 
 runTests :-
     forall('/bootstrap#testHasFailed'(Test, _, File, Result), (write(File), write(': '), write(Test), write(' --> '), write(Result), nl)).
+
+storeTerm(Term, ID) :-
+    chooseID(ID),
+    assert(storedTerm(ID, Term)).
+
+chooseID(ID) :-
+    random_between(0, 2000000000, ID),
+    \+ storedTerm(ID, _), !.
+chooseID(ID) :- chooseID(ID).
+
+continue(ID, RetVal) :-
+    retract(storedTerm(ID, cont(RetVal, Continuation, Res))),
+    evalImpred(Res, Continuation).
+
+mustSucceed(Goal) :-
+    Goal, !.
+mustSucceed(Goal) :-
+    throw(failed(Goal)).
+
+writeTerm(Term) :-
+    write_term(Term, [quoted(true), ignore_ops(true)]).
