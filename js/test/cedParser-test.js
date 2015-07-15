f937c48e38b4f8547d668ba565f6576f89052f57
Boaz Rosenan <brosenan@gmail.com>
2015-07-15 18:22:20 +0300

    cedParser begins

---
+++ js/test/cedParser-test.js
@@ -0,0 +1,15 @@
+"use strict";
+
+var assert = require("assert");
+var cedParser = require("../cedParser.js");
+
+describe('CedParser', function(){
+    var parser = new cedParser.CedParser();
+    describe('.parse(str)', function(){
+	it('should resolve a Cedalion string to a JS string', function(){
+	    
+	});
+
+    });
+
+});
