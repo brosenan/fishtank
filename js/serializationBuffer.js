1a3cbded03fcc7501ca78396766418765cf5a528
Boaz Rosenan <brosenan@gmail.com>
2015-09-03 21:06:40 +0300

    inventory + serializationBuffer

---
+++ js/serializationBuffer.js
@@ -0,0 +1,43 @@
+"use strict";
+module.exports = function(buff) {
+    this.buff = buff;
+    this.writePtr = 0;
+    this.readPtr = 0;
+};
+
+var proto = module.exports.prototype;
+
+proto.writeInt = function(num, width) {
+    this.writePtr = this.buff.writeIntBE(num, this.writePtr, width);
+}
+
+proto.writeInt32 = function(num) {
+    this.writeInt(num, 4);
+};
+proto.writeInt24 = function(num) {
+    this.writeInt(num, 3);
+};
+
+proto.readInt = function(width) {
+    var ret = this.buff.readIntBE(this.readPtr, width);
+    this.readPtr += width;
+    return ret;
+};
+proto.readInt32 = function() {
+    return this.readInt(4);
+};
+proto.readInt24 = function() {
+    return this.readInt(3);
+};
+
+proto.writeString = function(str) {
+    var len = this.buff.write(str, this.writePtr + 2, 'utf8');
+    this.writeInt(len, 2);
+    this.writePtr += len;
+};
+
+proto.readString = function() {
+    var len = this.readInt(2);
+    this.readPtr += len;
+    return this.buff.toString('utf8', this.readPtr - len, this.readPtr);
+};
---
+++ js/test/seiralizationBuffer-test.js
@@ -0,0 +1,59 @@
+"use strict";
+var assert = require('assert');
+
+var SerializationBuffer = require('../serializationBuffer.js');
+
+describe('SerializationBuffer', function(){
+    describe('.writeInt32(num)', function(){
+	it('should serialize a number as a 32 bit integer', function(){
+	    var buff = new SerializationBuffer(new Buffer(100));
+	    buff.writeInt32(100);
+	    buff.writeInt32(10000);
+	    buff.writeInt32(1000000);
+	    assert.equal(buff.readInt32(), 100);
+	});
+    });
+    describe('.readInt32()', function(){
+	it('should de-serialize int32 values', function(){
+	    var buff = new SerializationBuffer(new Buffer(100));
+	    buff.writeInt32(100);
+	    buff.writeInt32(10000);
+	    buff.writeInt32(1000000);
+	    assert.equal(buff.readInt32(), 100);
+	    assert.equal(buff.readInt32(), 10000);
+	    assert.equal(buff.readInt32(), 1000000);
+	});
+    });
+
+    describe('.writeInt24(num)', function(){
+	it('should serialize a number as a 32 bit integer', function(){
+	    var buff = new SerializationBuffer(new Buffer(100));
+	    buff.writeInt24(100);
+	    buff.writeInt24(10000);
+	    buff.writeInt24(1000000);
+	    assert.equal(buff.readInt24(), 100);
+	});
+    });
+    describe('.readInt24()', function(){
+	it('should de-serialize int32 values', function(){
+	    var buff = new SerializationBuffer(new Buffer(100));
+	    buff.writeInt24(100);
+	    buff.writeInt24(10000);
+	    buff.writeInt24(1000000);
+	    assert.equal(buff.readInt24(), 100);
+	    assert.equal(buff.readInt24(), 10000);
+	    assert.equal(buff.readInt24(), 1000000);
+	});
+    });
+    describe('.writeString(str)', function(){
+	it('should serialize a string', function(){
+	    var buff = new SerializationBuffer(new Buffer(100));
+	    buff.writeString('hello');
+	    buff.writeString('world');
+	    assert.equal(buff.readString(), 'hello');
+	    assert.equal(buff.readString(), 'world');
+	});
+
+    });
+
+});
