86c14b286df42f44cfbdd69a6e17ba7d4f1ce087
Boaz Rosenan <brosenan@gmail.com>
2015-11-18 13:54:39 +0200

    http using express.js

---
+++ js/http.js
@@ -0,0 +1,33 @@
+"use strict";
+var express = require('express');
+var $S = require('suspend'), $R = $S.resume, $RR = $S.resumeRaw, $T = function(gen) { return function(done) { $S.run(gen, done); } };
+
+var Nodalion = require('./nodalion.js');
+var ns = Nodalion.namespace('/nodalion', ['serveHandlers']);
+
+var setupRouter = $S.async(function*(router, nodalion, app) {
+    var Path = {var:'Path'};
+    var Handlers = {var:'Handlers'};
+    var Ctx = {var:'Ctx'};
+    var locations = yield nodalion.findAll([Path, Handlers, Ctx], ns.serveHandlers(app, Path, Ctx, Handlers), $R());
+    locations.forEach(function(loc) {
+	router.get(loc[0], loc[1]);
+    });
+});
+
+exports.app = function(nodalion, app) {
+    var router = express.Router();
+    setupRouter(router, nodalion, app, function(err) {
+	if(err) {
+	    console.error(err);
+	}
+    });
+    return router;
+};
+
+ns._register('outputText', function(contentType, text) {
+    return function(req, res) {
+	res.set('Content-Type', contentType);
+	res.send(text);
+    };
+});
---
+++ js/test/http-test.js
@@ -0,0 +1,30 @@
+"use strict";
+var assert = require('assert');
+var $S = require('suspend'), $R = $S.resume, $RR = $S.resumeRaw, $T = function(gen) { return function(done) { $S.run(gen, done); } };
+var express = require('express');
+var request = require('request');
+
+var Nodalion = require('../nodalion.js');
+var nodalionHttp = require('../http.js');
+
+var ns = Nodalion.namespace('/nodalion', ['http']);
+var example = Nodalion.namespace('example', ['myApp']);
+
+var nodalion = new Nodalion('/tmp/http.log');
+
+describe('http', function(){
+    describe('.app(nodalion, app)', function(){
+	it('should return an Express router', $T(function*(){
+	    var app = express();
+	    app.use(nodalionHttp.app(nodalion, example.myApp()));
+	    app.listen(3001);
+	    yield setTimeout($R(), 10); // Give the app time to go up
+	    var resp = yield request('http://localhost:3001/hello', $RR());
+	    assert.ifError(resp[0]);
+	    assert.equal(resp[1].statusCode, 200);
+	    assert.equal(resp[2], 'Hello, World');
+	}));
+
+    });
+
+});
